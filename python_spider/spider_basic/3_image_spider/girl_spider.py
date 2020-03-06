42b31c7e79ef6336207a183d0bce3f37b273ba88
刘兆锋 <1957875073@qq.com>
2020-03-06 09:23:09 +0800

    image_spider

--- python_spider/spider_basic/3_image_spider/girl_spider.py
+++ python_spider/spider_basic/3_image_spider/girl_spider.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/6 9:19
+# @Author : 技术TA说
+# @File : girl_spider.py
+# @Software: PyCharm
+
 import requests
 import re
 import os
@@ -5,11 +11,15 @@ from urllib import error
 
 
 def main():
+    # 图片存储路径
     dirPath = "girl-images"
+    # 目标网页
+    # https: // www.buxiuse.com /?page = 9
     base_url = "https://www.buxiuse.com/?page={}"
+    # 页数从2开始
     page = 2
-    j = 0
-    # while page < 11:
+    # 图片名称序号
+    image_id = 0
     for page in range(2, 12):
         url = base_url.format(page)
         try:
@@ -26,13 +36,13 @@ def main():
             else:
                 for enum in list:
                     image = requests.get(enum, timeout=7)
-                    filePath = os.path.join(dirPath, "girl_image_" + str(j) + ".jpg")
-                    print_str = '这是第 {} 个妹子,妹子我爱你'
-                    print(print_str.format(j))
+                    filePath = os.path.join(dirPath, "girl_image_" + str(image_id) + ".jpg")
+                    print_str = '这是第 {} 位小姐姐'
+                    print(print_str.format(image_id))
                     f = open(filePath, 'wb')
                     f.write(image.content)
                     f.close()
-                    j += 1
+                    image_id += 1
                 page += 1
 
 
